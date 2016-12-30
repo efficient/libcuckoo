a70fde816e62b4640e409e4a67f1c2ce604f8ca3
Manu Goyal <manu.goyal2013@gmail.com>
2016-12-30 13:58:59 -0600

    Removed extra testing code
    This stuff isn't part of the libcuckoo distribution.

--- tests/universal-benchmark/universal_table_wrapper.hh
+++ tests/universal-benchmark/universal_table_wrapper.hh
@@ -59,73 +59,7 @@ private:
 };
 
 #else
-#ifdef TBB
-#define TABLE "TBB"
-#define TABLE_TYPE "tbb::concurrent_hash_map"
-#include <tbb/concurrent_hash_map.h>
-
-struct CustomHashCompare {
-    static size_t hash(const KEY& k) {
-        return hashfn(k);
-    }
-
-    static bool equal(const KEY& k1, const KEY& k2) {
-        return k1 == k2;
-    }
-private:
-    static std::hash<KEY> hashfn;
-};
-
-std::hash<KEY> CustomHashCompare::hashfn;
-
-class Table {
-public:
-    Table(size_t n): tbl(n) {}
-
-    bool read(const KEY& k, VALUE& v) const {
-        typename decltype(tbl)::const_accessor a;
-        if (tbl.find(a, k)) {
-            v = a->second;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    bool insert(const KEY& k, const VALUE& v) {
-        return tbl.insert(std::make_pair(k, v));
-    }
-
-    bool erase(const KEY& k) {
-        return tbl.erase(k);
-    }
-
-    bool update(const KEY& k, const VALUE& v) {
-        typename decltype(tbl)::accessor a;
-        if (tbl.find(a, k)) {
-            a->second = v;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    template <typename Updater>
-    void upsert(const KEY& k, Updater fn, const VALUE& v) {
-        typename decltype(tbl)::accessor a;
-        if (tbl.insert(a, k)) {
-            a->second = v;
-        } else {
-            fn(a->second);
-        }
-    }
-
-    tbb::concurrent_hash_map<KEY, VALUE, CustomHashCompare> tbl;
-};
-
-#else
-#error Must define either LIBCUCKOO or TBB
-#endif
+#error Must define LIBCUCKOO
 #endif
 
 #endif // _UNIVERSAL_TABLE_WRAPPER_HH
